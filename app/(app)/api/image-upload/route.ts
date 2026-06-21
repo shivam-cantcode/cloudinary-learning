5e2535c292dc6b95b2a07c0b81436b111ad40104
shiv <154681480+shivam-cantcode@users.noreply.github.com>
2026-06-21 23:24:48 +0530

    added cloudinary

---
+++ app/(app)/api/image-upload/route.ts
@@ -0,0 +1,45 @@
+import { v2 as cloudinary } from "cloudinary";
+import { NextResponse, NextRequest } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUDNAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.NODE_ENV, // Click 'View API Keys' above to copy your API secret
+});
+
+interface CloudinaryUploadResult {
+  public_id: string;
+  [key: string]: any;
+}
+
+export async function POST(request: NextRequest) {
+  const { userId } = await auth();
+  if (!userId) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+  try {
+    const formData = await request.formData();
+
+    const file = formData.get("file") as File | null;
+    if (!file) {
+      return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
+    }
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+    await new Promise<CloudinaryUploadResult>((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        { folder: "next-cloudinary-uploads" },
+        (error, result) => {
+          if (error) {
+            reject(error);
+          } else if (result) {
+            resolve(result);
+          }
+        },
+      );
+      uploadStream.end(buffer);
+    });
+  } catch (error) {}
+}
